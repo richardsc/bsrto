c796f6bca35a67ad2767d72ab26c50b304b083cd
Clark Richards <clark.richards@gmail.com>
2017-12-04 11:34:30 -0400

    shore station barometer processing

---
+++ baro.R
@@ -0,0 +1,14 @@
+rm(list=ls())
+library(oce)
+
+datadir <- '/data/archive/barrow/2017/bsrto/hpb/'
+
+system(paste0('cat ', datadir, '* > baro.hpb'))
+
+baro <- read.table('baro.hpb', stringsAsFactors = FALSE,
+                col.names = c('date', 'time', 'patm', 'Tatm'))
+baro$time <- as.POSIXct(paste(baro$date, baro$time), format='%m/%d/%Y %H:%M:%S', tz='UTC')
+baro$patm <- baro$patm/10 # convert to kPa
+
+save(file='baro.rda', baro)
+
