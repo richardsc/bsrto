0eb1388e1e0ef82d359e1e8fadb9e24dc13d243d
Clark Richards <clark.richards@gmail.com>
2017-12-04 23:36:59 -0400

    processing for met data

--- update_data.sh
+++ update_data.sh
@@ -2,5 +2,6 @@
 R --no-save < download_data.R > download_data.out
 R --no-save < mc.R > mc.out
 R --no-save < icl.R > icl.out
+R --no-save < met.R > met.out
 R --no-save < baro.R > baro.out
 R --no-save < ips.R > ips.out
