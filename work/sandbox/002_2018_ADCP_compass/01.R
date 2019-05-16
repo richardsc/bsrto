de01470d26a04d8530a3e073cda3eba3f08be649
Clark Richards <clark.richards@gmail.com>
2019-05-15 22:56:59 -0300

    estimate ADCP heading from ADCP/PC relationship

--- work/sandbox/002_2018_ADCP_compass/01.R
+++ work/sandbox/002_2018_ADCP_compass/01.R
@@ -1,6 +1,8 @@
 library(oce)
+pl <- oce.plot.ts
 load('../../../adp.rda')
 load('../../../pc.rda')
+adpFull <- adp
 
 focus <- as.POSIXct(c('2018-08-29 02:00:00', '2019-02-17 04:08:00'), tz='UTC')
 adp <- subset(adp, focus[1] <= time & time <= focus[2])
@@ -11,11 +13,14 @@ dec <- magneticField(rep(lon, length(t)), rep(lat, length(t)), t)$declination
 
 hpc <- adp[['heading']]
 hadp <- adp[['headingOriginal']] + dec
-
 ## clean up the ADCP heading to unwrap negatives and angles with large differences from the PC
 hadp[hadp < 0] <- hadp[hadp < 0] + 360
 II <- abs(hadp - hpc) > 180
 hadp[II] <- hadp[II] + 360
+II <- hpc > 360
+hpc[II] <- hpc[II] - 360
+hadp[II] <- hadp[II] - 360
+
 ## plot(hpc, hadp)
 
 if (!interactive()) pdf('01.pdf')
@@ -48,5 +53,56 @@ grid()
 
 hist(hpc - (hadp + dec), 100,
      main='', xlab=expression(Delta*Heading))
+
+hsf <- function(x, hsp) {
+    approx(hsp$y, hsp$x, x)$y
+}
+
+d <- subset(adpFull, focus[2] <= time & time <= as.POSIXct('2019-08-01'))
+tt <- d[['time']]
+hh <- d[['headingOriginal']] + magneticField(rep(lon, length(tt)), rep(lat, length(tt)), tt)$declination
+hhh <- hsf(hh, hsp)
+pl(t, hpc, xlim=c(focus[1], as.POSIXct('2019-08-01')))
+lines(tt, hhh, col=2)
+
+hist(hpc, 100, col=1, main='', xlab='Heading')
+hist(hhh, 100, add=TRUE, col=2)
+legend('topleft', c('Pole Compass', 'Pole compass fit'), lty=1, col=1:2)
+
+
+adpFull[['heading']] <- c(hpc, hhh)
+enu <- toEnu(adpFull)
+
+plot(enu, which=1:2)
+
+## plot(enu, which=19:20)
+
+time <- enu[['time']]
+e <- apply(enu[['v']][,,1], 1, mean, na.rm=TRUE)
+n <- apply(enu[['v']][,,2], 1, mean, na.rm=TRUE)
+## ef <- lowpass(e, n=25)
+## nf <- lowpass(n, n=25)
+ef <- approx(time, lowpass(e, n=25), time)$y
+nf <- approx(time, lowpass(n, n=25), time)$y
+par(mfrow=c(2, 1))
+pl(time, e)
+grid()
+lines(time, ef, col=2, lwd=2)
+pl(time, n)
+grid()
+lines(time, nf, col=2, lwd=2)
+par(mfrow=c(1, 1))
+
+par(mfrow=c(2, 1))
+pl(time, e, xlim=as.POSIXct(c('2019-02-01', '2019-03-01'), tz='UTC'))
+grid()
+lines(time, ef, col=2, lwd=2)
+abline(v=focus[2], lwd=2)
+pl(time, n, xlim=as.POSIXct(c('2019-02-01', '2019-03-01'), tz='UTC'))
+grid()
+lines(time, nf, col=2, lwd=2)
+abline(v=focus[2], lwd=2)
+par(mfrow=c(1, 1))
+
 
 if (!interactive()) dev.off()
