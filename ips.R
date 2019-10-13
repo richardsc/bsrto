a8716a57d69b7bf5ef40b84cba4702b69de96e6e
Clark Richards <clark.richards@gmail.com>
2019-10-12 23:49:55 -0300

    update ips processing for 2019 files and met now works (after oce change)

--- ips.R
+++ ips.R
@@ -34,69 +34,76 @@ i <- 1
 ips <- list()
 for (file in files) {
     con <- file(file)
-    d <- readLines(con)
+    dd <- readLines(con)
     close(con)
-    time <- numberAsPOSIXct(unlist(strsplit(d[1], ' '))[1])
-    if (time < as.POSIXct('2018-01-01', tz='UTC')) {
-        time  <- as.POSIXct(paste(unlist(strsplit(d[1], ' '))[-(1:2)], collapse=' '),
-                            format='%b %d %H:%M:%S %Y', tz='UTC')
-    }
-    tmp <- as.numeric(unlist(strsplit(d[3], ' ')))
-    maxDraft <- tmp[1]
-    minDraft <- tmp[2]
-    meanDraft <- tmp[3]
-    sdDraft <- tmp[4]
-    tmp <- as.numeric(unlist(strsplit(d[4], ' ')))
-    nRanges <- tmp[1]
-    nPartialRanges <- tmp[2]
-    soundSpeed <- tmp[3]
-    ## barometricPressure <- tmp[4]
-    density <- tmp[4]
-    gravity <- tmp[5]
-    tmp <- as.numeric(unlist(strsplit(d[5], ' ')))
-    maxPressure <- tmp[1]
-    minPressure <- tmp[2]
-    maxTemperature <- tmp[3]
-    minTemperature <- tmp[4]
-    tmp <- as.numeric(unlist(strsplit(d[6], ' ')))
-    maxPitch <- tmp[1]
-    maxRollPitch <- tmp[2]
-    maxRoll <- tmp[3]
-    maxPitchRoll <- tmp[4]
-    maxInclination <- tmp[5]
-    bins <- as.numeric(unlist(strsplit(d[7], ',')))
-    if (!any(is.na(bins))) {
-        minRange <- 9
-        maxRange <- 22
-        dRange <- 0.1
-        rangeBin <- seq(9, 22-dRange, dRange)
-        rangeHist <- bins[1:130]
-        ## construct a time series according to the ranges
-        range <- NULL
-        for (ii in seq_along(rangeHist)) {
-            range <- c(range, rep(rangeBin[ii], rangeHist[ii]))
+    ## find the spaces between 2 hour histograms. Should be the same
+    ## everytime, but let's search it to be safe
+    chunkIndex <- c(0, which(dd == '')) #add the first line, and drop the last
+    for (ii in head(seq_along(chunkIndex), -1)) {
+        d <- dd[(chunkIndex[ii]+1):(chunkIndex[ii+1])]
+        time <- numberAsPOSIXct(unlist(strsplit(d[1], ' '))[1])
+        if (time < as.POSIXct('2018-01-01', tz='UTC')) {
+            time  <- as.POSIXct(paste(unlist(strsplit(d[1], ' '))[-(1:2)], collapse=' '),
+                                format='%b %d %H:%M:%S %Y', tz='UTC')
         }
-        ## FIXME: not yet including the above/below bin values
-        ## correct for patm (should use Hub barometer)
-        ## range <- range - barometricPressure/100
-        ##
-        ## determine average barometric pressure for the 6 hour period from the
-        ## shore station barometer
-        II <- time <= baro$time & baro$time <= time + 6*3600
-        barometricPressure <- mean(baro$patm[II], na.rm=TRUE)
-        if (i > 1 & is.nan(barometricPressure)) barometricPressure <- ips[[i-1]]$barometricPressure # take the last value
-        ## correct all the patm dependent fields
-        
-        ips[[i]] <- list(time=time, barometricPressure=barometricPressure,
-                         density=density,
-                         maxDraft=maxDraft, meanDraft=meanDraft,
-                         minDraft=minDraft, sdDraft=sdDraft, maxPressure=maxPressure,
-                         minPressure=minPressure, maxTemperature=maxTemperature,
-                         minTemperature=minTemperature, maxPitch=maxPitch, maxRoll=maxRoll,
-                         maxRollPitch=maxRollPitch, maxPitchRoll=maxPitchRoll,
-                         rangeBin=rangeBin, rangeHist=rangeHist, range=range)
+        tmp <- as.numeric(unlist(strsplit(d[3], ' ')))
+        maxDraft <- tmp[1]
+        minDraft <- tmp[2]
+        meanDraft <- tmp[3]
+        sdDraft <- tmp[4]
+        tmp <- as.numeric(unlist(strsplit(d[4], ' ')))
+        nRanges <- tmp[1]
+        nPartialRanges <- tmp[2]
+        soundSpeed <- tmp[3]
+        ## barometricPressure <- tmp[4]
+        density <- tmp[4]
+        gravity <- tmp[5]
+        tmp <- as.numeric(unlist(strsplit(d[5], ' ')))
+        maxPressure <- tmp[1]
+        minPressure <- tmp[2]
+        maxTemperature <- tmp[3]
+        minTemperature <- tmp[4]
+        tmp <- as.numeric(unlist(strsplit(d[6], ' ')))
+        maxPitch <- tmp[1]
+        maxRollPitch <- tmp[2]
+        maxRoll <- tmp[3]
+        maxPitchRoll <- tmp[4]
+        maxInclination <- tmp[5]
+        bins <- as.numeric(unlist(strsplit(d[7], ',')))
+        if (!any(is.na(bins))) {
+            minRange <- 9
+            maxRange <- 22
+            dRange <- 0.1
+            rangeBin <- seq(9, 22-dRange, dRange)
+            rangeHist <- bins[1:130]
+            ## construct a time series according to the ranges
+            range <- NULL
+            for (ii in seq_along(rangeHist)) {
+                range <- c(range, rep(rangeBin[ii], rangeHist[ii]))
+            }
+            ## FIXME: not yet including the above/below bin values
+            ## correct for patm (should use Hub barometer)
+            ## range <- range - barometricPressure/100
+            ##
+            ## determine average barometric pressure for the 6 hour period from the
+            ## shore station barometer
+            II <- time <= baro$time & baro$time <= time + 6*3600
+            barometricPressure <- mean(baro$patm[II], na.rm=TRUE)
+            if (i > 1 & is.nan(barometricPressure)) barometricPressure <- ips[[i-1]]$barometricPressure # take the last value
+            ## correct all the patm dependent fields
+            
+            ips[[i]] <- list(time=time, barometricPressure=barometricPressure,
+                             density=density,
+                             maxDraft=maxDraft, meanDraft=meanDraft,
+                             minDraft=minDraft, sdDraft=sdDraft, maxPressure=maxPressure,
+                             minPressure=minPressure, maxTemperature=maxTemperature,
+                             minTemperature=minTemperature, maxPitch=maxPitch, maxRoll=maxRoll,
+                             maxRollPitch=maxRollPitch, maxPitchRoll=maxPitchRoll,
+                             rangeBin=rangeBin, rangeHist=rangeHist, range=range)
+        }
+        i <- i + 1
     }
-    i <- i + 1
 }
+
 save(file='ips.rda', ips)
 
--- update_data.sh
+++ update_data.sh
@@ -2,7 +2,7 @@
 LANG=C.UTF-8 R --no-save < download_data.R > download_data.out
 LANG=C.UTF-8 R --no-save < mc.R > mc.out
 LANG=C.UTF-8 R --no-save < icl.R > icl.out
-##LANG=C.UTF-8 R --no-save < met.R > met.out
+LANG=C.UTF-8 R --no-save < met.R > met.out
 LANG=C.UTF-8 R --no-save < baro.R > baro.out
 LANG=C.UTF-8 R --no-save < ips.R > ips.out
 LANG=C.UTF-8 R --no-save < pc.R > pc.out
